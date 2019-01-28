6509cc52bc241f5a8aacdf6095be2db12284ad51
josh <lowejosh50@gmail.com>
2019-01-29 04:29:26 +1100

    added working binary encoding

--- js/script.js
+++ js/script.js
@@ -10,32 +10,38 @@ function denco(dom, encFunc, decFunc) {
 }
 
 // Create all denco objects and place them into the global array for easier manipulation
-taArray.push(new denco(document.getElementById("ascii"), encodeASCII(), decodeASCII()));
-taArray.push(new denco(document.getElementById("binary"), encodeBinary(), decodeBinary()));
-taArray.push(new denco(document.getElementById("hexadecimal"), encodeHexadecimal(), decodeHexadecimal()));
-taArray.push(new denco(document.getElementById("decimal"), encodeDecimal(), decodeDecimal()));
-taArray.push(new denco(document.getElementById("base64"), encodeBase64(), decodeBase64()));
-taArray.push(new denco(document.getElementById("url"), encodeURL(), decodeURL()));
+taArray.push(new denco(document.getElementById("ascii"), encodeASCII, decodeASCII));
+taArray.push(new denco(document.getElementById("binary"), encodeBinary, decodeBinary));
+taArray.push(new denco(document.getElementById("hexadecimal"), encodeHexadecimal, decodeHexadecimal));
+taArray.push(new denco(document.getElementById("decimal"), encodeDecimal, decodeDecimal));
+taArray.push(new denco(document.getElementById("base64"), encodeBase64, decodeBase64));
+taArray.push(new denco(document.getElementById("url"), encodeURL, decodeURL));
 
 // Update the text buffer whenever a textarea is changed
 function textChanged(inputID) {
     console.log("working: " + taArray[inputID].dom.id);
     textBuffer = taArray[inputID].dom.value;               // Update text buffer
 
+    console.log("tb: " + textBuffer);
+    console.log("binary: " + encodeBinary(textBuffer));
     // Force text buffer onto all other textarea DOM elements
+    // TODO - update with encodings/decodings
     for (let i = 0; i < taArray.length; i++) {
         taArray[i].dom.value = textBuffer;
     }
     
 }
 
-// Encoding functions
-function encodeASCII() {
-
+// Encoding functions (All are developed under the context of receiving ASCII input)
+function encodeASCII(input) {
+    // Do nothing
+    return input;
 }
 
-function encodeBinary() {
-
+function encodeBinary(input) {
+    return input.split('').map(function (char) {
+        return char.charCodeAt(0).toString(2);
+    }).join(' ');
 }
 
 function encodeHexadecimal() {
@@ -54,9 +60,10 @@ function encodeURL() {
 
 }
 
-// Decoding functions
-function decodeASCII() {
-
+// Decoding functions (All are developed under the context of returning ASCII output)
+function decodeASCII(input) {
+    // Do nothing
+    return input;
 }
 
 function decodeBinary() {
