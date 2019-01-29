1c4ceda019337c9da6093c39a4c7a10b774f8f22
josh <lowejosh50@gmail.com>
2019-01-30 01:40:23 +1100

    added binary decoding

--- js/script.js
+++ js/script.js
@@ -21,12 +21,11 @@ taArray.push(new denco(document.getElementById("url"), encodeURL, decodeURL));
 function textChanged(inputID) {
     textBuffer = taArray[inputID].decFunc(taArray[inputID].dom.value);               // Decode to ASCII and update text buffer
 
-    console.log("binary 0010: " + decodeBinary("01010010 00101110"));
-    console.log("encoding a: " + encodeBinary("a"));
     // Force text buffer onto all other textarea DOM elements
-    for (let i = 0; i < taArray.length; i++) {
+    for (i = 0; i < taArray.length; i++) {
         // If the input isn't the one currently being edited
         if (i != inputID) {
+            // Select the object and encode the text buffer onto it's DOM value
             let obj = taArray[i];
             obj.dom.value = obj.encFunc(textBuffer);
         }
@@ -39,13 +38,16 @@ function encodeASCII(input) {
 }
 
 function encodeBinary(input) {
+    // Split the input to be encoded character by character
     return input.split('').map(function (char) {
+        // Find the character's numeric value and convert it to base 2
         let binary = char.charCodeAt(0).toString(2);   
         // Add the zeroes that were removed
         while (binary.length < 8) {
             binary = '0' + binary; 
         }
         return binary;
+    // Join the strings - separated by spaces
     }).join(' ');
 }
 
@@ -75,6 +77,14 @@ function decodeASCII(input) {
 }
 
 function decodeBinary(input) {
+    console.log("STR: " + input);
+    let binary = input.split(" ");
+    let ascii = [];
+
+    for (i = 0; i < binary.length; i++) {
+        ascii.push(String.fromCharCode(parseInt(binary[i], 2)));
+    }
+    return ascii.join("");
 }
 
 function decodeHexadecimal() {
